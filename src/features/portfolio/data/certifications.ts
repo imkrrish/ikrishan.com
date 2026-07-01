faad23f028ddd86018c9733bea3527406a51f557
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2026-07-01 08:39:30 +0530

    quick fixes

--- src/features/portfolio/data/certifications.ts
+++ src/features/portfolio/data/certifications.ts
@@ -4,7 +4,7 @@ export const CERTIFICATIONS: Certification[] = [
   {
     title: "The Complete Full-Stack Web Development Bootcamp",
     issuer: "Dr. Agngela Yu, Instructor",
-    issueDate: "2026-02-04",
+    issueDate: "2020-12-16",
     credentialID: "UC-9c6ce552-8b37-4a84-9c84-d9a94493609d",
     credentialURL:
       "https://www.udemy.com/certificate/UC-9c6ce552-8b37-4a84-9c84-d9a94493609d/",
--- src/features/portfolio/data/social-links.ts
+++ src/features/portfolio/data/social-links.ts
@@ -2,44 +2,44 @@ import type { SocialLink } from "../types/social-links"
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    icon: "https://assets.ikrishan.com/images/link-icons/linkedin.webp",
+    icon: "https://assets.ikrishan.com/images/linkedin.webp",
     title: "LinkedIn",
     subtitle: "imkrrish",
     href: "https://www.linkedin.com/in/imkrrish",
   },
   {
-    icon: "https://assets.ikrishan.com/images/link-icons/github.webp",
+    icon: "https://assets.ikrishan.com/images/github.webp",
     title: "GitHub",
     subtitle: "imkrrish",
     href: "https://github.com/imkrrish",
   },
   {
-    icon: "https://assets.ikrishan.com/images/link-icons/x.webp",
+    icon: "https://assets.ikrishan.com/images/x.webp",
     title: "X",
     subtitle: "@_Krrish_001",
     href: "https://x.com/_Krrish_001",
   },
 
   // {
-  //   icon: "https://assets.ikrishan.com/images/link-icons/dailydotdev.webp",
+  //   icon: "https://assets.ikrishan.com/images/dailydotdev.webp",
   //   title: "daily.dev",
   //   subtitle: "@ikrishan",
   //   href: "https://app.daily.dev/ikrishan",
   // },
   // {
-  //   icon: "https://assets.ikrishan.com/images/link-icons/discord.webp",
+  //   icon: "https://assets.ikrishan.com/images/discord.webp",
   //   title: "Discord",
   //   subtitle: "ikrishan",
   //   href: "https://discord.com/",
   // },
   // {
-  //   icon: "https://assets.ikrishan.com/images/link-icons/zalo.webp",
+  //   icon: "https://assets.ikrishan.com/images/zalo.webp",
   //   title: "Zalo",
   //   subtitle: "Quaric",
   //   href: "https://zalo.me",
   // },
   // {
-  //   icon: "https://assets.ikrishan.com/images/link-icons/youtube.webp",
+  //   icon: "https://assets.ikrishan.com/images/youtube.webp",
   //   title: "YouTube",
   //   subtitle: "@ikrishan",
   //   href: "https://www.youtube.com/@ikrishan",
